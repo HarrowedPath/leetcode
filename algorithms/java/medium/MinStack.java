381d93478307f7ad8e9d9b26017dc92b92afaa96
Lalloo <beeaveer@gmail.com>
2023-02-19 15:51:04 +0300

    Two New Problems

---
+++ algorithms/java/medium/MinStack.java
@@ -0,0 +1,35 @@
+package medium;
+
+import java.util.Stack;
+
+public class MinStack {
+    private int min;
+    private final Stack<Integer> stack;
+
+    public MinStack() {
+        min = Integer.MAX_VALUE;
+        stack = new Stack<>();
+    }
+
+    public void push(int val) {
+        if (val <= min) {
+            stack.push(min);
+            min = val;
+        }
+        stack.push(val);
+    }
+
+    public void pop() {
+        if (stack.pop() == min) {
+            min = stack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min;
+    }
+}
